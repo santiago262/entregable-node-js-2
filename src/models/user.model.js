7a60df49ca4055840617bfcafdcbeb478a978a9c
santiago262 <vizcainogarcia262@gmail.com>
2024-05-06 15:08:41 -0500

    v1

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,11 +1,12 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
-
-const sequelize = new Sequelize('api_users', 'root', '1234', {
-    host: 'localhost',
-    dialect: "mysql",
-    port:3306
-  });
-
+const password="BQ0o1JivcitLQpkoH73g9TlMmklZWu9c"
+const username="api_users_d7of_user"
+const database ="api_users_d7of"
+const sequelize = new Sequelize( database, username, password, {
+    host: 'dpg-cosjapicn0vc738gmjqg-a',
+    dialect: 'postgres',  
+    port: 5432          
+})
 
 class User extends Model {}
 User.init({
